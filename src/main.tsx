ea252c587d9369cb589b45e8c1cbf62ee614a412
aksel26 <kevinxkim2023@gmail.com>
2025-07-23 14:59:55 +0900

    다국어 관련 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRouter from './AppRouter.tsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRouter from "./AppRouter.tsx";
+import "./i18n";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
-  </StrictMode>,
-)
+    <Suspense fallback="loading...">
+      <AppRouter />
+    </Suspense>
+  </StrictMode>
+);
